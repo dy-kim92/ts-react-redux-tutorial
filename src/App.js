8528b273cfa0b5d5a2048a3acfcfcd3779ef20b5
dongyeon.kim <dongyeon.kim@lasee.io>
2023-03-10 09:30:43 +0900

    모듈 통합

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
-import CounterContainer from './containers/CounterContainer'
-import Counter from './components/Counter';
 import TodoInsert from './components/TodoInsert';
 import TodoList from './components/TodoList';
 
-
+//
 function App() {
   return (
     <>
-      <Counter />
+      <TodoInsert />
+      <TodoList />
     </>
   );
 }
---
+++ src/modules/todos/index.ts
@@ -0,0 +1,99 @@
+export { default } from './reducer';
+export * from './actions';
+export * from './types';
+
+// //  todos 리덕스 모듈 리팩토링
+// import { createAction, ActionType, createReducer } from 'typesafe-actions';
+
+// //  액션 type 기존코드
+// // const ADD_TODO = 'todos/ADD_TODO' as const;
+// // const TOGGLE_TODO = 'todos/TOGGLE_TODO' as const;
+// // const REMOVE_TODO = 'todos/REMOVE_TODO' as const;
+
+// const ADD_TODO = 'todos/ADD_TODO';
+// const TOGGLE_TODO = 'todos/TOGGLE_TODO';
+// const REMOVE_TODO = 'todos/REMOVE_TODO';
+
+
+// //  액션 생성 함수 기존코드
+// // export const addTodo = (text : string) => ({
+// //     type : ADD_TODO,
+// //     payload : text
+// // })
+// // export const toggleTodo = (text : number) => ({
+// //     type : TOGGLE_TODO,
+// //     payload : text
+// // })
+// // export const removeTodo = (text : number) => ({
+// //     type : REMOVE_TODO,
+// //     payload : text
+// // })
+
+// export const addTodo = createAction(ADD_TODO)<string>();
+// export const toggleTodo = createAction(TOGGLE_TODO)<number>();
+// export const removeTodo = createAction(REMOVE_TODO)<number>();
+
+
+// //  액션들의 타입스크립트 타입 준비 기존코드
+// // type TodosAction = 
+// //     | ReturnType<typeof addTodo>
+// //     | ReturnType<typeof toggleTodo>
+// //     | ReturnType<typeof removeTodo>;
+
+// const actions = { addTodo, toggleTodo, removeTodo };
+// type TodosAction = ActionType<typeof actions>;
+
+
+// //  상태를 위한 타입 선언
+// export type Todo = {
+//     id : number;
+//     text : string;
+//     done : boolean;
+// }
+// type TodosState = Todo[];
+
+// //  초기값 설정
+// const initialState : TodosState = [
+//     { id: 1, text: '타입스크립트 배우기', done: true },
+//     { id: 2, text: '타입스크립트와 리덕스 함께 사용해보기', done: true },
+//     { id: 3, text: '투두리스트 만들기', done: false }
+// ]
+
+
+// //  리듀서 구현 기존코드
+// // function todos(state : TodosState = initialState, action : TodosAction) : TodosState {
+// //     switch (action.type) {
+// //         case ADD_TODO:
+// //           const nextId = Math.max(...state.map(todo => todo.id)) + 1;
+// //           return state.concat({
+// //             id: nextId,
+// //             text: action.payload,
+// //             done: false
+// //           });
+// //         case TOGGLE_TODO:
+// //           return state.map(todo =>
+// //             todo.id === action.payload ? { ...todo, done: !todo.done } : todo
+// //           );
+// //         case REMOVE_TODO:
+// //           return state.filter(todo => todo.id !== action.payload);
+// //         default:
+// //           return state;
+// //     }
+// // }
+
+// //  createReducer 로 리듀서 구현
+// const todos = createReducer<TodosState, TodosAction> (initialState, {
+//   [ADD_TODO] : (state, { payload : text }) =>
+//     state.concat({
+//       id : Math.max(...state.map(todo => todo.id)) + 1,
+//       text,
+//       done : false
+//     }),
+//   [TOGGLE_TODO] : (state, { payload : id }) =>
+//     state.map(todo => (todo.id === id ? { ...todo, done : !todo.done} : todo)),
+//   [REMOVE_TODO] : (state, { payload : id }) =>
+//     state.filter(todo => todo.id !== id)
+// });
+
+
+// export default todos;
